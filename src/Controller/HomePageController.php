6dfbb5192a73047c85d7bc880b082f909de3bd4b
vagrant <vagrant@homestead>
2018-05-20 18:17:18 +0000

    first commit

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomePageController extends Controller
+{
+    /**
+     * @Route("/", name="home_page")
+     */
+    public function index()
+    {
+        return $this->render('home_page/index.html.twig', [
+            'controller_name' => 'HomePageController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin", name="admin_area")
+     */
+    public function adminArea()
+    {
+        return new Response('Admin area');
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    public function setEmail($email)
+    {
+        $this->setUsername($email);
+
+        return parent::setEmail($email);
+    }
+}
